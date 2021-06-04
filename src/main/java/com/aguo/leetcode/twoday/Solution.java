ab9bcbcf6aadf7ac77c89568b8886f40df3675ee
Code Fruit <61573357+1260839205@users.noreply.github.com>
2021-06-04 19:55:25 +0800

    Code Fruit

---
+++ src/main/java/com/aguo/leetcode/twoday/Solution.java
@@ -0,0 +1,23 @@
+package com.aguo.leetcode.twoday;
+
+/**
+ * @Author Code Fruit
+ * @Email 1260839205@qq.com
+ * @Date 2021/6/4 下午7:10
+ */
+
+ //Definition for singly-linked list.
+class ListNode {
+    int val;
+     ListNode next;
+     ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+}
